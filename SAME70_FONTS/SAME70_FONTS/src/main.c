107659ebc939e4f9137baaca8ce188223bfa83a2
GiuPassarelli <gb_passarelli@hotmail.com>
2019-04-08 16:57:05 -0300

    Tempo

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -16,6 +16,14 @@
 #define BUT_PIO_IDX       4u
 #define BUT_PIO_IDX_MASK  (1u << BUT_PIO_IDX)
 
+#define HOUR        15
+#define MINUTE      30
+#define SECOND      50
+
+volatile uint8_t flag_time = 1;
+
+void RTC_init(void);
+
 
 struct ili9488_opt_t g_ili9488_display_opt;
 
@@ -32,17 +40,28 @@ void configure_lcd(void){
 	
 }
 
-void button_init(void){
-	// Initialize the board clock
-	sysclk_init();
-	
-	// Desativa WatchDog Timer
-	WDT->WDT_MR = WDT_MR_WDDIS;
+/**
+*  Handle Interrupcao botao 1
+*/
+static void Button1_Handler(uint32_t id, uint32_t mask)
+{
 
+}
+
+void button_init(void){
+	/* config. pino botao em modo de entrada */
 	pmc_enable_periph_clk(BUT_PIO_ID);
-	
-	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_PULLUP);
-	pio_pull_up(BUT_PIO, BUT_PIO_IDX_MASK, 1);
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
+
+	/* config. interrupcao em borda de descida no botao do kit */
+	/* indica funcao (but_Handler) a ser chamada quando houver uma interrupção */
+	pio_enable_interrupt(BUT_PIO, BUT_PIO_IDX_MASK);
+	pio_handler_set(BUT_PIO, BUT_PIO_ID, BUT_PIO_IDX_MASK, PIO_IT_FALL_EDGE, Button1_Handler);
+
+	/* habilita interrupçcão do PIO que controla o botao */
+	/* e configura sua prioridade                        */
+	NVIC_EnableIRQ(BUT_PIO_ID);
+	NVIC_SetPriority(BUT_PIO_ID, 1);
 }
 
 
@@ -60,22 +79,102 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 	}	
 }
 
+void RTC_Handler(void)
+{
+
+	uint32_t minute;
+	uint32_t second;
+	uint32_t hour;
+	
+	rtc_get_time(RTC, &hour, &minute, &second);
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/*
+	*  Verifica por qual motivo entrou
+	*  na interrupcao, se foi por segundo
+	*  ou Alarm
+	*/
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {
+			if(second == 60){
+				second = 0;
+				minute += 1;
+			}
+			rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+			
+			rtc_set_time_alarm(RTC, 1, hour, 1, minute, 1, second+1);
+	}
+	
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+	
+}
+
+void RTC_init(){
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(RTC, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_time(RTC, HOUR, MINUTE, SECOND);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(RTC_IRQn);
+	NVIC_ClearPendingIRQ(RTC_IRQn);
+	NVIC_SetPriority(RTC_IRQn, 0);
+	NVIC_EnableIRQ(RTC_IRQn);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(RTC,  RTC_IER_ALREN);
+
+}
+
 
 int main(void) {
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
+	uint32_t minute;
+	uint32_t second;
+	uint32_t hour;
+	
 	button_init();
+	RTC_init();
 	board_init();
 	sysclk_init();	
 	configure_lcd();
+	char bufferHour[32];
+	char bufferMinute[32];	
+	char bufferSecond[32];
 	
 	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
 	
 	while(1) {
 		
+		rtc_get_time(RTC, &hour, &minute, &second);
+		
+		sprintf(bufferHour, "%d", hour);
+		sprintf(bufferMinute, "%d", minute);
+		sprintf(bufferSecond, "%d", second);
+		
+		font_draw_text(&calibri_36, bufferHour, 40, 200, 1);
+		font_draw_text(&calibri_36, ":", 80, 200, 1);
+		font_draw_text(&calibri_36, bufferMinute, 120, 200, 1);
+		font_draw_text(&calibri_36, ":", 160, 200, 1);
+		font_draw_text(&calibri_36, bufferSecond, 200, 200, 1);
+		
 		if(!(pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)))
 		{
 			delay_ms(100);
-			font_draw_text(&arial_72, "102456", 50, 200, 2);
+			
 		}
 		
 	}
